b1fbc96a054cfcd678a238b6646576b678e6a2eb
Somu Bhargava <somubhargava97@gmail.com>
2016-10-04 09:41:44 +0000

    gui_r1

---
+++ Source/frontend/php_scripts_version1/datasubtract.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+
+$username = "root";
+
+$password = "";
+
+$dbname = "opencart";
+$conn=new mysqli($servername, $username, $password, $dbname);
+if($conn)
+{
+	$var=$_POST['customer_ID'];
+	$fin=$_POST['product_ID'];
+	$dec=$_POST['items'];
+	$sql="select  table_name from user_records where customer_id=$var";
+	$result=$conn->query($sql);	
+	$row=$result->fetch_assoc();
+	$table=$row['table_name'];
+	$sql="select * from $table where product_id=$fin";
+	$result=$conn->query($sql);
+	$i=0;	
+	while($row=$result->fetch_assoc())
+	{
+		$i=$i+1;
+		$ele=$row["quantity"];			
+	}
+	$dec=$ele-$dec;
+	 $sql="update $table set quantity=$dec where  product_id=$fin";
+	$result=$conn->query($sql);						
+}
+else
+{
+	echo "Not connected";
+}
+
+?>
